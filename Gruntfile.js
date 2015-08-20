e992d8f4a44c3be8aacf05bf05c66fe84beae943
xavier <xavier@ekkocreatives.com>
2015-08-20 16:40:31 +1000

    Added loadTask

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,13 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+
     qunit: {
       all: ['test/index.html']
     }
   });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-qunit');
 
   grunt.registerTask('test', ['qunit']);
   grunt.registerTask('default', ['test']);
